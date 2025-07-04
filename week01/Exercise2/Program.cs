e78c1aa85bb2da8486cf748e25f14f7288d46089
dannystarf <danielajayi1012042004@gmail.com>
2025-07-03 20:49:34 -1200

    Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string gradeLetter = "";
+        string sign = "";
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+
+        else
+        {
+            gradeLetter = "F";
+        }
+        
+        int remainder = gradePercentage % 10;
+
+        if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (gradePercentage >= 90 && remainder >= 7)
+        {
+            sign = "";
+        }
+
+        if (gradePercentage < 60)
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"You Grade is {gradeLetter}{sign}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry You did not pass the class. Try again next time!");
+        }
+
     }
 }
