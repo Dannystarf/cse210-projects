43fa97a5ef4ad6a369224b26f0cbd5a6d24e779c
dannystarf <danielajayi1012042004@gmail.com>
2025-08-11 15:39:37 -1200

    mindfulness

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+        _prompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?",
+            "What are things you are grateful for?",
+            "What are your favorite memories?",
+            "What accomplishments are you proud of?"
+        };
+    }
+
+    public override void RunActivity()
+    {
+        _count = 0;
+        Random random = new Random();
+        string prompt = GetRandomPrompt(random);
+
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine($" --- {prompt} ---");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string response = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(response))
+            {
+                _count++;
+            }
+        }
+
+        Console.WriteLine($"You listed {_count} items!");
+    }
+
+    public string GetRandomPrompt(Random random)
+    {
+        return _prompts[random.Next(_prompts.Count)];
+    }
+}
